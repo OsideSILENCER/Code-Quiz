d7b9a9363a90fce78cac4435011434410aaecd1c
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-04-05 23:40:18 -0700

    Add all changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,59 @@
-var introduction = document.querySelector("#Introduction")
+var introduction = document.querySelector("#introduction")
 introduction.addEventListener("click", function (event) {
-    alert("You clicked the Submit button");
+    alert("You clicked the Submit button")
     var btn = event.target;
     if (btn.matches(".choice-btn")) {
-
     }
 
 });
-
-var questionEl = document.querySelector("#question2")
-questionEl.addEventListener("click", function (event) {
-    alert("You Answered Correctly!");
-    var btn = event.target;
-    if (btn.matches(".choice-btn")) {
-var answer = btn.parentElement.getAttribute("data-answer");
-    }
-
-});
-
-
-
-
-
-
-
-
-
-
-
 
 document.querySelector("#submit").addEventListener("click", function () {
-window.location.href = "./questions.html";
-});
+    window.location.href = "./questions.html";
+    });
+    
+var ss = document.getElementsByClassName("countingtimer");
+ 
+[].forEach.call(ss, function (s) {
+    var currentTimer = 0,
+    interval = 0,
+    lastUpdateTime = getTime()
+    start = s.querySelector('buttton.start' ),
+    secs = s.querySelector('span.seconds'),
+
+start.addEventListener("click", startTimer)
+
+    function pad (n) {
+        return ('00' + n).substring(-1)
+    }
+
+    function update () {
+        var now = new getTime(),
+        dt = now - lastUpdateTime;
+
+        currentTimer += dt;
+
+        var Time = new(counter)
+
+        secs.innerHTML = pad(time.getSeconds());
+
+        lastUpdateTime = now;
+
+    }
+
+    function startTimer () {
+        clearInterval(interval);
+        interval = 0;
+    }
+
+    function resetTimer () {
+        stopTimer();
+
+        currentTimer = 0
+
+        secs.innerHTML = pad(0);
+    }
+})
+
 
 var timer = document.querySelector('h1');
 let timerSeconds = 20;
@@ -45,6 +67,68 @@ if (timerSeconds<0 || timerSeconds<1){
     clearInterval(counter)
     }
 }, 1000)
+
+var introduction = document.querySelector("#Introduction")
+introduction.addEventListener("click", function (event) {
+    alert("You clicked the Submit button");
+    var btn = event.target;
+    if (btn.matches(".choice-btn")) {
+    }
+
+});
+
+   
+
+var questionEl = document.querySelector("#question1");
+questionEl.addEventListener("click", function (event) {
+    var btn = event.target;
+    if (btn.matches(".choice-btn")) {
+        alert("You answered correctly");
+    }
+
+
+});
+
+var questionEl = document.querySelector("#question2");
+questionEl.addEventListener("click", function (event) {
+    var btn = event.target;
+    if (btn.matches(".choice-btn")) {
+        alert("You answered correctly");
+    }
+
+
+});
+
+
+var questionEl = document.querySelector("#question3");
+questionEl.addEventListener("click", function (event) {
+    var btn = event.target;
+    if (btn.matches(".choice-btn")) {
+        alert("You answered correctly");
+    }
+
+
+});
+
+
+var questionEl = document.querySelector("#question4");
+questionEl.addEventListener("click", function (event) {
+    var btn = event.target;
+    if (btn.matches(".choice-btn")) {
+        alert("You answered correctly");
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
 
 
 
